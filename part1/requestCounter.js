43cf775b4b2fccd8e6b02ef8c1be55530a01571e
lucasjellema <lucasjellema@gmail.com>
2017-05-21 08:38:28 +0200

    Simple, naive, non-scalable requestcounter

---
+++ part1/requestCounter.js
@@ -0,0 +1,12 @@
+//respond to HTTP requests with static response
+// invoke from browser or using curl:  curl http://127.0.0.1:3000
+var http = require('http');
+
+var requestCounter =0 ;
+
+var server = http.createServer(function handleRequest(req, res) {
+    res.write('Request Count: '+ ++requestCounter);
+    res.end();
+}).listen(3000);
+
+console.log('server running on port 3000');
