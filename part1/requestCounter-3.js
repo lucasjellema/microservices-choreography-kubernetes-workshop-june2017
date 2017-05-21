2c7b224dfc2315e8f511fb89f2110f00bb94f003
lucasjellema <lucasjellema@gmail.com>
2017-05-21 14:26:25 +0200

    Added requestcounter3 powered by Redis cache

---
+++ part1/requestCounter-3.js
@@ -0,0 +1,36 @@
+//respond to HTTP requests with response: count of number of requests
+// invoke from browser or using curl:  curl http://127.0.0.1:PORT
+var http = require('http');
+var redis = require("redis");
+
+var redisHost = process.env.REDIS_HOST ||"192.168.99.100" ;
+var redisPort = process.env.REDIS_PORT ||6379;
+
+var redisClient = redis.createClient({ "host": redisHost, "port": redisPort });
+
+var PORT = process.env.APP_PORT || 3000;
+
+var server = http.createServer(function handleRequest(req, res) {
+    var requestCounter = 0;
+
+    redisClient.get("requestCounter", function (err, reply) {
+        if (err) {
+            res.write('Request Count (Version 3): ERROR ' + err);
+            res.end();
+        } else {
+            if (!reply || reply == null) {
+                console.log("no value found yet");
+                redisClient.set("requestCounter", requestCounter);
+            } else {
+                requestCounter = Number(reply) + 1;
+                redisClient.set("requestCounter", requestCounter);
+            }
+            res.write('Request Count (Version 3): ' + requestCounter);
+            res.end();
+        }
+    })
+}).listen(PORT);
+
+    //        redisClient.quit();
+
+console.log('Node.JS Server running on port ' + PORT + ' for version 3 of requestCounter application, powered by Redis.');
