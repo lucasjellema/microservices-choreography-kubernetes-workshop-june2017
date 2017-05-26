ccf47c677645160684d9364a4e4a67d1ac32593a
lucasjellema <lucasjellema@gmail.com>
2017-05-26 06:52:54 +0200

    -

--- part3/node-kafka/KafkaConsumer.js
+++ part3/node-kafka/KafkaConsumer.js
@@ -1,12 +1,12 @@
 var kafka = require('kafka-node')
 var Consumer = kafka.Consumer
 
-var KAFKA_ZOOKEEPER_HOST = process.env.KAFKA_ZOOKEEPER_HOST;
-var KAFKA_ZOOKEEPER_PORT = process.env.KAFKA_ZOOKEEPER_PORT;
+var KAFKA_ZOOKEEPER_HOST = process.env.KAFKA_ZOOKEEPER_HOST || '192.168.99.100';
+var KAFKA_ZOOKEEPER_PORT = process.env.KAFKA_ZOOKEEPER_PORT || '30686';
 
-var KAFKA_TOPIC = process.env.KAFKA_TOPIC;
-
-console.log("KafkaConsumer reporting for duty");
+var KAFKA_TOPIC = process.env.KAFKA_TOPIC || 'event-topic';
+var version = 1.6;
+console.log("KafkaConsumer (version " + version + ") reporting for duty");
 console.log("KAFKA_ZOOKEEPER_HOST: " + KAFKA_ZOOKEEPER_HOST);
 console.log("KAFKA_ZOOKEEPER_PORT: " + KAFKA_ZOOKEEPER_PORT);
 console.log("KAFKA_TOPIC: " + KAFKA_TOPIC);
@@ -16,6 +16,12 @@ var kafkaConnectDescriptor = KAFKA_ZOOKEEPER_HOST + ":" + KAFKA_ZOOKEEPER_PORT;
 var eventBusTopic = KAFKA_TOPIC;
 console.log('Creating Client');
 var client = new kafka.Client(kafkaConnectDescriptor)
+
+console.log("Try to retrieve list of topics:");
+client.zk.client.getChildren("/brokers/topics", (err, children, stats) => {
+  children.forEach(child => console.log(child));
+});
+
 
 var consumer = new Consumer(
   client,
