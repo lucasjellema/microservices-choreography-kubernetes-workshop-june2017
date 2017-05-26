a9b48ea0f0afe62902d99ea770f4f2d4dc52e2af
lucasjellema <lucasjellema@gmail.com>
2017-05-26 06:32:23 +0200

    Version added

--- part3/node-kafka/KafkaProducer.js
+++ part3/node-kafka/KafkaProducer.js
@@ -3,18 +3,19 @@
 
 var kafka = require('kafka-node')
 var Producer = kafka.Producer
-var KAFKA_ZOOKEEPER_HOST = process.env.KAFKA_ZOOKEEPER_HOST || 'kafka-zoo-svc';
-var KAFKA_ZOOKEEPER_PORT = process.env.KAFKA_ZOOKEEPER_PORT || '2181';
+var KAFKA_ZOOKEEPER_HOST = process.env.KAFKA_ZOOKEEPER_HOST || '192.168.99.100';
+var KAFKA_ZOOKEEPER_PORT = process.env.KAFKA_ZOOKEEPER_PORT || '30686';
 
 var KAFKA_TOPIC = process.env.KAFKA_TOPIC || 'event-topic';
-
-console.log("KafkaProducer reporting for duty");
+var version = 1.4;
+console.log("KafkaProducer (version "+version+") reporting for duty");
 console.log("KAFKA_ZOOKEEPER_HOST: " + KAFKA_ZOOKEEPER_HOST);
 console.log("KAFKA_ZOOKEEPER_PORT: " + KAFKA_ZOOKEEPER_PORT);
 console.log("KAFKA_TOPIC: " + KAFKA_TOPIC);
 
 
 var kafkaConnectDescriptor = KAFKA_ZOOKEEPER_HOST + ":" + KAFKA_ZOOKEEPER_PORT;
+var eventBusTopic = KAFKA_TOPIC;
 
 var client = new kafka.Client(kafkaConnectDescriptor)
 var producer = new Producer(client);
