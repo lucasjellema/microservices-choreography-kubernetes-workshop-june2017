22ef79154ea7dd0df6354ab2e98acc93772c781b
lucasjellema <lucasjellema@gmail.com>
2017-05-26 06:18:47 +0200

    -

--- part3/node-kafka/KafkaConsumer.js
+++ part3/node-kafka/KafkaConsumer.js
@@ -7,22 +7,28 @@ var KAFKA_ZOOKEEPER_PORT = process.env.KAFKA_ZOOKEEPER_PORT;
 var KAFKA_TOPIC = process.env.KAFKA_TOPIC;
 
 console.log("KafkaConsumer reporting for duty");
+console.log("KAFKA_ZOOKEEPER_HOST: " + KAFKA_ZOOKEEPER_HOST);
+console.log("KAFKA_ZOOKEEPER_PORT: " + KAFKA_ZOOKEEPER_PORT);
+console.log("KAFKA_TOPIC: " + KAFKA_TOPIC);
+
 
 var kafkaConnectDescriptor = KAFKA_ZOOKEEPER_HOST + ":" + KAFKA_ZOOKEEPER_PORT;
 var eventBusTopic = KAFKA_TOPIC;
+console.log('Creating Client');
 var client = new kafka.Client(kafkaConnectDescriptor)
 
 var consumer = new Consumer(
   client,
   [],
-  {fromOffset: true}
+  { fromOffset: true }
 );
 
 consumer.on('message', function (message) {
   console.log("received message", message);
-  console.log("received message",JSON.stringify(message));
+  console.log("received message", JSON.stringify(message));
 });
 
+console.log("Going to add topic " + eventBusTopic);
 consumer.addTopics([
-  { topic: eventBusTopic, partitions: 1, offset: 0}
-], () => console.log("topic added"));
+  { topic: eventBusTopic, partitions: 1, offset: 0 }
+], () => console.log("topic " + eventBusTopic + " added"));
