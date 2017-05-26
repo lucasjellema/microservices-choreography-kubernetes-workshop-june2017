6296b7d6f2a5d810b6d36b12e69799c480ba93ae
lucasjellema <lucasjellema@gmail.com>
2017-05-26 07:31:42 +0200

    Add topic on broker

--- part3/node-kafka/KafkaProducer.js
+++ part3/node-kafka/KafkaProducer.js
@@ -7,7 +7,7 @@ var KAFKA_ZOOKEEPER_HOST = process.env.KAFKA_ZOOKEEPER_HOST || '192.168.99.100';
 var KAFKA_ZOOKEEPER_PORT = process.env.KAFKA_ZOOKEEPER_PORT || '30686';
 
 var KAFKA_TOPIC = process.env.KAFKA_TOPIC || 'event-topic';
-var version = 1.6;
+var version = 1.7;
 console.log("KafkaProducer (version " + version + ") reporting for duty");
 console.log("KAFKA_ZOOKEEPER_HOST: " + KAFKA_ZOOKEEPER_HOST);
 console.log("KAFKA_ZOOKEEPER_PORT: " + KAFKA_ZOOKEEPER_PORT);
@@ -22,31 +22,37 @@ var producer = new Producer(client);
 
 var topics = [];
 
+
+
 console.log("Try to retrieve list of topics:");
 client.zk.client.getChildren("/brokers/topics", (err, children, stats) => {
-    children.forEach(child => {console.log(child); topics.push(child)});
+    children.forEach(child => {
+        console.log(child); topics.push(child); console.log("Topics " + JSON.stringify(topics));
+    });
 });
 
 producer.on('ready', function () {
     console.log("producer  is ready");
 
-client.zk.client.getChildren("/brokers/topics", (err, children, stats) => {
-    children.forEach(child => {
-    KeyedMessage = kafka.KeyedMessage
-    km = new KeyedMessage('key', 'message'),
-        payloads = [
-            { topic: child, messages: 'hi from Windows Host', partitions: 0 },
-            { topic: child, messages: ['hi from node producer', 'one other message', km], partitions: 0 },
-        ];
-
-    producer.send(payloads, function (err, data) {
-        console.log("send is complete " + data);
-        console.log("error " + err);
+    producer.createTopics(['test-topic'], false, function (err, data) {
+        console.log(JSON.stringify(data));
     });
+    topics.forEach(topic => {
+        KeyedMessage = kafka.KeyedMessage
+        km = new KeyedMessage('key', 'message'),
+            payloads = [
+                { topic: topic, messages: 'hi from Windows Host', partitions: 0 },
+                { topic: topic, messages: ['hi from node producer', 'one other message', km], partitions: 0 },
+            ];
+        console.log('send to topic ' + topic);
+        producer.send(payloads, function (err, data) {
+            console.log("send is complete " + data);
+            console.log("error from sending " + err);
+        });
 
-    producer.on('error', function (err) {
-        console.error("Error " + err)
-    })
-    })//for each child
-})//getChildren
+        producer.on('error', function (err) {
+            console.error("Error " + err)
+        })
+    })//for each topic
+
 })
